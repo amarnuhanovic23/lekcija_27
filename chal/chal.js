b1214313f8bcc014bb97b0b33299407dd80ac70c
amarnuhanovic23 <amarmanji1234@gmail.com>
2026-05-05 21:00:32 +0200

    sve i challenge

---
+++ chal/chal.js
@@ -0,0 +1,22 @@
+function mintosec(minutes) {
+    return minutes * 60;
+}
+
+function areaOfTriangle(base, height) {
+    return (base * height) / 2;
+}
+
+mintosec(5);
+areaOfTriangle(5, 7);
+
+
+var me = {
+    name: "Amar",
+    age: 13,
+    city: "Tuzla",
+    country: "Bosnia and Herzegovina",
+    razred: function() {
+        console.log("7th grade");
+    }
+}
+ razred();
